35faf8574fdd8073ebe9c4082f041a2b56e01440
Guilherme Mendonca <guilhermeesmendoca@gmail.com>
2024-03-25 19:03:33 -0300

    sétimo commit do HTML e primeiro commit do JS

---
+++ script.js
@@ -0,0 +1,26 @@
+let conta = 0
+let pessoas = 0
+
+const contaInput = document.querySelector("#conta")
+contaInput.addEventListener("input", receberValorConta) /* Esse evento é acionado toda vez que o usuário mexe dentro do input*/
+
+function receberValorConta(evento) { /* "console.log(evento.target.value)"  Toda vez que o usuário digitar no input, a gente pega a informação value(valor) do que foi digitado*/
+    conta = Number(evento.target.value)
+}
+
+const pessoasInput = document.querySelector("#pessoas")
+pessoasInput.addEventListener("input", receberQuantidadePessoas)
+
+function receberQuantidadePessoas(evento) {
+const paragrafoErro = document.querySelector(".pessoas #erro")
+const divErro = document.querySelector(".pessoas .input-box")
+
+    if(evento.target.value === "0") {
+        paragrafoErro.style.display = "block"
+        divErro.setAttribute("id", "erro-div")
+    } else {
+        paragrafoErro.style.display = "none"
+        divErro.setAttribute("id", "")
+        pessoas = Number(evento.target.value)
+    }
+}
