275dd721f3e9e589d28e3a2bef2d96dbf1a56cc6
Guilherme Mendonca <guilhermeesmendoca@gmail.com>
2024-03-25 20:06:49 -0300

    sexto commit do JS e AGORA SIM PROJETO FINALIZADO

--- script.js
+++ script.js
@@ -81,4 +81,8 @@ function limpar() {
 
     document.querySelector(".gorjeta-total > strong").innerHTML = "R$ 0.00"
     document.querySelector(".total > strong").innerHTML = "R$ 0.00"
+
+    conta = 0
+    porcentagem = 0
+    pessoas = 0
 }
